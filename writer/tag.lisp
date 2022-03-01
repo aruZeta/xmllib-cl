de40bf4fdc4682c0e86e55eabc5ca1ca232ebd31
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-03-01 13:10:13 +0100

    [add] Macro to create xml structures

--- writer/tag.lisp
+++ writer/tag.lisp
@@ -6,7 +6,9 @@
            #:end-tag
            #:content
            #:tag
-           #:simple-tag))
+           #:simple-tag
+           #:xml-header-tag
+           #:write-xml))
 (in-package :xmllib-cl/writer/tag)
 
 (defvar *indent-level* 0)
@@ -86,3 +88,39 @@ If SIMPLEP is non-nil writes a simple tag without newlines nor tabs."
      ,(unless simplep
         '(fresh-line *output-stream*))
      (end-tag ,name :indentp ,(not simplep))))
+
+(defun xml-header-tag (name
+                       &key
+                         attrs
+                         (stream *output-stream*))
+  "Write a xml header tag NAME to STREAM with attributes ATTRS.
+ATTRS is a LIST of LIST with 2 items, the name and value of an attribute."
+  (format stream
+          "<?~A~:{ ~A=\"~A\"\~}?>"
+          name
+          attrs))
+
+(defmacro write-xml ((stream
+                      &key
+                        stylesheet
+                        (version "1.0" versionp)
+                        (encoding "UTF-8" encodingp))
+                     &body body)
+  "Write a xml structure to STREAM.
+STYLESHEET is a STRING specifying the stylesheet linked to the xml if non-nil.
+VERSION is a STRING specifying the version of the xml.
+ENCODING is a STRING specifying the encoding of the xml."
+  `(let ((*output-stream* ,stream))
+     (xml-header-tag "xml"
+                     :attrs '(("version" ,(if versionp
+                                              version
+                                              "1.0"))
+                              ("encoding" ,(if encodingp
+                                               encoding
+                                               "UTF-8"))))
+     (fresh-line *output-stream*)
+     ,(when stylesheet
+        `(xml-header-tag "xml-stylesheet"
+                         :attrs '(("href" ,stylesheet)
+                                  ("type" "text/xsl"))))
+     ,@body))
